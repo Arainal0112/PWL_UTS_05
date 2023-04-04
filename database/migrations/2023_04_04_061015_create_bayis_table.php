121df959b314c703260fba1f3fcc0d867b8962ff
Arainal0112 <arainalaldi0112@gmail.com>
2023-04-04 13:16:32 +0700

    1# first commit

---
+++ database/migrations/2023_04_04_061015_create_bayis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bayi', function (Blueprint $table) {
+            $table->integer('no_urut')->primary();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->string('tgl_lahir');
+            $table->string('bb_lahir');
+            $table->string('tb_lahir');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bayis');
+    }
+};
