42d8b006b656312b6a229c582791da2007fe622c
Arainal0112 <arainalaldi0112@gmail.com>
2023-04-04 20:24:35 +0700

    3# update view

--- resources/views/bayi/create.blade.php
+++ resources/views/bayi/create.blade.php
@@ -1,61 +1,71 @@
-@extends('mahasiswas.layout')
+@extends('layouts.content')
+
+@section('title', 'Data Bayi')
 
 @section('content')
+<x-sidebar home="active" profil=""/>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Dashboard</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
 
-<div class="container mt-5">
+    <!-- Main content -->
+    <section class="content">
 
-    <div class="row justify-content-center align-items-center">
-        <div class="card" style="width: 24rem;">
-            <div class="card-header">
-                Tambah Mahasiswa
-            </div>
-            <div class="card-body">
-                @if ($errors->any())
-                <div class="alert alert-danger">
-                    <strong>Whoops!</strong> There were some problems with your i
-                    nput.<br><br>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-                @endif
-                <form method="post" action="{{ route('mahasiswas.store') }}" id="myForm">
-                    @csrf
-                    <div class="form-group">
-                        <label for="Nim">Nim</label>
-                        <input type="text" name="nim" class="form-control" id="nim" aria-describedby="nim">
-                    </div>
-                    <div class="form-group">
-                        <label for="Nama">Nama</label>
-                        <input type="Nama" name="nama" class="form-control" id="nama" aria-describedby="nama">
-                    </div>
-                    <div class="form-group">
-                        <label for="ttl">Tanggal Lahir</label>
-                        <input type="date" name="ttl" class="form-control" id="ttl" aria-describedby="TTL" value='2000-01-01'>
-                    </div>
-                    <div class="form-group">
-                        <label for="kelas">Kelas</label>
-                        <input type="kelas" name="kelas" class="form-control" id="kelas" aria-describedby="password">
-                    </div>
-                    <div class="form-group">
-                        <label for="jurusan">Jurusan</label>
-                        <input type="jurusan" name="jurusan" class="form-control" id="jurusan" aria-describedby="jurusan">
-                    </div>
-                    <div class="form-group">
-                        <label for="email">email</label>
-                        <input type="email" name="email" class="form-control" id="email" aria-describedby="Email">
-                    </div>
-                    <div class="form-group">
-                        <label for="no_hp">No Handphone</label>
+        <!-- Default box -->
+        <div class="col-lg-12 margin-tb">
+        <div class="pull-left mt-2">
+            <h2>DATA BAYI</h2>
+        </div>
+        <div class="float-left my-2">
+            <form action="" method="GET" class="d-flex">
+                <input type="text" class="form-control" name="nama" placeholder="Nama Bayi" value="" required>
+                <button type="submit" class="btn btn-primary">Search</button>
 
-                        <input type="no_hp" name="no_hp" class="form-control" id="no_hp" aria-describedby="no_hp">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
+            </form>
+
+        </div>
+        <div class="float-right my-2">
+            <a class="btn btn-success" href="{{ route('bayi.create') }}"> Input BAYI</a>
         </div>
     </div>
+    @if ($message = Session::get('success'))
+<div class="alert alert-success">
+    <p>{{ $message }}</p>
 </div>
-@endsection
+@endif
+
+<table class="table table-bordered">
+    <tr>
+
+<th>Nim</th>
+        <th>No Urut</th>
+        <th>Nama</th>
+        <th>Tanggal Lahir</th>
+        <th>Berat</th>
+        <th>Tinggi</th>
+        <th width="280px">Action</th>
+    </tr>
+</table>
+<div class="d-flex">
+
+</div>
+        <!-- /.card -->
+
+    </section>
+    @endsection
